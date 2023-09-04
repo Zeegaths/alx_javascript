62bb55565fd22b8d5ab086b5d526947d5287b775
Zeegaths <zarahgathoni76@gmail.com>
2023-09-04 22:44:27 +0300

    value.

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // replacing 89 with 12
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  let value = 89
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+value.myObject = 89;
+console.log(myObject);
