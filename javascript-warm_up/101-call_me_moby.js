69c313970729c2916f02f1afde484204ed2fffdb
Zeegaths <zarahgathoni76@gmail.com>
2023-09-06 11:37:11 +0300

    self

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,5 @@
 
 // excecutes x times a fnction
-function theFunction(x) {
-    for (let i = 0; i < x; i++)
-    theFunction();
-}
-function callMeMoby (x, theFunction) {
-    return x * theFunction()
+function callMeMoby (x, callMeMoby) {
+    return x * callMeMoby();
 }
