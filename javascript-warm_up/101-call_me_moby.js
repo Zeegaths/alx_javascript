9312d8d762fed0c182397920d191025dd304b3fb
Zeegaths <zarahgathoni76@gmail.com>
2023-09-06 11:57:44 +0300

    moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,11 @@
 // excecutes x times a fnction
+function callMeMoby(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+        theFunction();
+    }
+}
+
+// Call the callMeMoby function with the specified arguments
 callMeMoby(3, function () {
     console.log('C is fun');
-  });
+});
