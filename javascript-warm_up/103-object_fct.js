a6dc8b7477a5a0f103526573d6616d3a4ad56db0
Zeegaths <zarahgathoni76@gmail.com>
2023-09-07 13:13:40 +0300

    incr

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,13 +1,20 @@
 // the function increments and calls a function
 const myObject = {
     type: 'object',
-    value: 12,
-    incr: function() {
-      this.value++;
-    }
+    value: 12
+  };  
+  console.log(myObject);
+
+  function incr() {
+    myObject.value++;    
+  }
+
+  myObject.incr = function () {
+    myObject.value++;
   };
-  
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  
+
+  for (let i = 0; i < 3;i++){
+    myObject.incr();
+    console.log(myObject);
+  }
+   
