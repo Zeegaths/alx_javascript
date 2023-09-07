5f4452679a7d19b425827e6a982b8466d5745bd9
Zeegaths <zarahgathoni76@gmail.com>
2023-09-07 11:23:20 +0300

    inctreented number

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,7 @@
 // the function increments and calls a function
-function addMeMaybe (number, theFunction){
-    return number++; 
-    addMeMaybe(theFunction);
+function addMeMaybe(number, theFunction) {
+    const incrementedNumber = number + 1;
+    theFunction(incrementedNumber);
 }
-module.exports = {addMeMaybe};
+
+module.exports = { addMeMaybe };
