1fe006a622b30a6bcb6c8af8c9206f07846ae11f
Zeegaths <zarahgathoni76@gmail.com>
2023-09-07 11:58:49 +0300

    incr

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -3,8 +3,9 @@ function addMeMaybe(number, theFunction) {
     const incrementedNumber = number + 1;
     theFunction(incrementedNumber);
 }
+
 function incr(number) {
-    return number + 1
-};
-console.log(number);
-module.exports = { addMeMaybe, incr };
+    return number + 1;
+}
+
+
