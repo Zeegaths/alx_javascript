db61d982c7c37198c182a936c88c07cef329305f
Zeegaths <zarahgathoni76@gmail.com>
2023-09-07 11:48:17 +0300

    console

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,14 +1,17 @@
+// the function increments and calls a function
 function addMeMaybe(number, theFunction) {
     const incrementedNumber = number + 1;
     const result = {
         value: incrementedNumber,
         incr: incr
     };
+    console.log(result);
     theFunction(result);
 }
 
 function incr(number) {
-    return number + 1;
+    return number.value + 1;
 }
 
-module.exports = { addMeMaybe };
+module.exports = { addMeMaybe, incr };
+
