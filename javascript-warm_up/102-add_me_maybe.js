84b2d9b1eb4870bf7c7cb9003d4c5ef01e31f0f1
Zeegaths <zarahgathoni76@gmail.com>
2023-09-07 11:26:06 +0300

    incr

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -3,5 +3,8 @@ function addMeMaybe(number, theFunction) {
     const incrementedNumber = number + 1;
     theFunction(incrementedNumber);
 }
+function incr(number) {
+    return number + 1;
+}
 
 module.exports = { addMeMaybe };
