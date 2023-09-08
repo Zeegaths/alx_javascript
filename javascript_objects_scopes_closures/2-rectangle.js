f59ce21ca473349773140fa64ee81a3f32d92775
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 04:27:52 +0300

    if

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+// This class defines a rectangle
+const Rectangle = class {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w
+            this.height = h
+        }
+    }
+
+}
+
+module.exports = Rectangle;
+
