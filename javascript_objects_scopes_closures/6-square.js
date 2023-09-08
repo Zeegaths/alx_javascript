d5c7ca5742c2f1c80eba0be8be41f0b83f55dd19
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 16:03:13 +0300

    undefined

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 // Creates a square
 class Square extends require('./5-square') {
-      charPrint(c) {
-      if (typeof c === undefined) {
-        c = "X";      
-      }
-      for (let i = 0; i < this.height; i++){
-        console.log(c.repeat(this.width));
-      }
+    charPrint(c) {
+        if (c === undefined) {
+            c = "X";
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width));
+        }
     }
-  }
-  
-  module.exports = Square;
-  
-  
+}
+
+module.exports = Square;
+
