24d3537d1ad788208f97a2465081eb8158f858fa
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 04:34:23 +0300

    xx

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// This class defines a rectangle
+const Rectangle = class {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            let row = '';
+            for (let j = 0; j < this.width; j++) {
+                row += 'X';
+            }
+            console.log(row);
+        }
+    }
+};
+
+module.exports = Rectangle;
