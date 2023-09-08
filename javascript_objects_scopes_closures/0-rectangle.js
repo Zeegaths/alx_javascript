1501887adf582addad579a9005ddd036530993ad
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 03:44:23 +0300

    example

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,8 @@
 // This class defines a rectangle
 class Rectangle {
+  // Constructor can be added if needed to initialize properties or methods
+}
 
-};
-  
+// Example usage:
+const r1 = new Rectangle();
+console.log(r1); // This should output: Rectangle {}
