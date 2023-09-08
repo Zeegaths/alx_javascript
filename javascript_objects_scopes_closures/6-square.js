aa58022e44e9437f2d54331906b46815e595803d
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 14:57:16 +0300

    for

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,15 @@
 #!/usr/bin/node
 // Creates a square
 class Square extends require('./5-square') {
-    constructor(size) {
-      super(size);
-    }
-  
     charPrint(c = 'X') {
-      if (typeof c !== 'string' || c.length !== 1) {
-        throw new Error('The argument c must be a single character string.');
-      }
-  
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+        if (typeof c === undefined || c.length !== 1) {
+            c = "X";
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width));
+        }
     }
-  }
-  
-  module.exports = Square;
-  
-  
+}
+
+module.exports = Square;
+
