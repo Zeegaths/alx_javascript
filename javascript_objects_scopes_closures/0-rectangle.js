2fac4327e24487cb46cb68ad4c06bb05197886ac
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 03:59:58 +0300

    shebang

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,6 @@
+#!/usr/bin/node
 // This class defines a rectangle
 class Rectangle {
   // Constructor can be added if needed to initialize properties or methods
 }
-// Example usage:
-const r1 = new Rectangle();
-console.log(r1); // This should output: Rectangle {}
-console.log(Rectangle);
+
