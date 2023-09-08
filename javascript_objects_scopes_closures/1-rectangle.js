a410bdd4972f42266e6654bcde7f30e4818fb3ea
Zeegaths <zarahgathoni76@gmail.com>
2023-09-08 04:19:24 +0300

    task 1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+// This class defines a rectangle
+const Rectangle = class {
+    constructor(w, h) {
+        this.width = w
+        this.height = h
+    }
+  }
+  
+  module.exports = Rectangle;
+  
+  
