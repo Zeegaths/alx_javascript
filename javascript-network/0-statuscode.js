80640192f25b2ae908f527b15172dba6db7b9404
Zeegaths <zarahgathoni76@gmail.com>
2023-09-11 16:52:45 +0300

    changed request to req

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,7 +3,7 @@
 const req = require('request');
 const url = `https://intranet.alxswe.com/status`;
 
-request.get(url, { encoding: 'utf-8' })
+req.get(url, { encoding: 'utf-8' })
     .on('data', data => {
         console.log(data);
     })
