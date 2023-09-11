ec4e24210fc7790cd2fed2aea2d52957d666337b
Zeegaths <zarahgathoni76@gmail.com>
2023-09-11 22:20:58 +0300

    chat

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,7 +1,31 @@
 #!/usr/bin/node
-// gets contents of a webpage and storesit ins file
 const request = require('request');
 const fs = require('fs');
-const apiUrl = process.argv[2];
 
-request.get(apiUrl).pipe(fs.createWriteStream('response.json'));
+if (process.argv.length !== 4) {
+    console.log('Usage: node getWebPageContents.js <URL> <file path>');
+    process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, { encoding: 'utf-8' }, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Error:', response.statusCode);
+        process.exit(1);
+    }
+
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (writeError) => {
+        if (writeError) {
+            console.error('Error writing to file:', writeError);
+            process.exit(1);
+        }
+        console.log(`Webpage content has been saved to "${filePath}"`);
+    });
+});
