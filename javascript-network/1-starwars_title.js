87542f795383fa2f5ffb28e94ab12b55c0150ec5
Zeegaths <zarahgathoni76@gmail.com>
2023-09-11 20:43:17 +0300

    request

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
-// prints the title of a star wa movie to match a given integer
+// prints the title of a star wars movie to match a given integer
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/:id`;
 
 request.get(url, function (error, response, body) {
-    const response = JSON.parse(data);
-    console.log('Title:', body.title)
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+
+    const responseData = JSON.parse(body); // Use a different variable name here
+    console.log('Title:', responseData.title);
 });
 
